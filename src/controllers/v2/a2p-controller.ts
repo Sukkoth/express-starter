1bc24da2d4f96110034b7b025e07421ea17e2ddb
Sukkoth <suukootj@gmail.com>
2025-04-16 14:11:27 +0300

    add chunk and encoding checker util to the controller

--- src/controllers/v2/a2p-controller.ts
+++ src/controllers/v2/a2p-controller.ts
@@ -1,8 +1,9 @@
 import asyncHandler from 'express-async-handler';
-import { parsePhone } from '@libs/parse-phone';
 import { a2pSchema } from '@utils/validation/a2p-schema';
 import validate from '@utils/validation/validate';
 import messagingService from '@services/messaging-service';
+import { checkMessageChunkAndEncoding } from '@utils/chunks';
+import { randomUUID } from 'node:crypto';
 
 /**
  * A2P (Application-to-Person) message controller.
@@ -13,20 +14,19 @@ import messagingService from '@services/messaging-service';
  *
  * @route POST /api/v2/a2p
  * @access Protected (requires auth middleware)
- *
- * @param {Express.Request} req - The incoming request object.
- * @param {Express.Response} res - The response object used to send the result.
- *
- * @returns {Object} JSON response:
- * - `message`: Status message
- * - `id`: Placeholder message ID
- * - `phone`: Target recipient phone number
- * - `provider`: Detected provider (`ethio_telecom` | `safaricom`)
  */
 
 export const a2pController = asyncHandler(async (req, res) => {
+  //validate and throw if invalid
   const data = validate(a2pSchema, req.body);
 
+  //check message chunks and encoding
+  const { encoding, chunks } = checkMessageChunkAndEncoding({
+    message: data.text,
+    encoding: data.smsEncoding,
+  });
+
+  //add to queue
   await messagingService.addToQueue({
     data,
     serviceType: 'A2P',
@@ -34,10 +34,13 @@ export const a2pController = asyncHandler(async (req, res) => {
   });
 
   res.json({
+    success: true,
     message: 'message queued successfully',
-    id: '1234567890',
-    phone: data.to,
-    provider: parsePhone(data.to).data!.provider,
-    callbackUrl: data.callbackUrl,
+    data: {
+      to: data.to,
+      smsEncoding: encoding,
+      chunks,
+      messageId: randomUUID(),
+    },
   });
 });
