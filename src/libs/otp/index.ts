538f9e59656809e28ac2f933e9e1625440d226b0
Sukkoth <suukootj@gmail.com>
2025-04-17 14:04:04 +0300

    sample otp generator

---
+++ src/libs/otp/index.ts
@@ -0,0 +1,28 @@
+import { OtpCharacters } from '@/types/enums';
+
+/**
+ * @param length - The length of the OTP to generate.
+ * @param charSet - The character set to use for the OTP.
+ * @returns The generated OTP as a string.
+ */
+function generateOtp(length: number, charSet: OtpCharacters) {
+  const characters = {
+    [OtpCharacters.numeric]: '0123456789',
+    [OtpCharacters.alphabetic]:
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    [OtpCharacters.alphanumeric]:
+      '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  };
+
+  const characterSet = characters[charSet];
+  let otp = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characterSet.length);
+    otp += characterSet[randomIndex];
+  }
+
+  return otp;
+}
+
+export default { generateOtp };
